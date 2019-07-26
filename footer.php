bb3d79da4e867fded255fc85e1a8bb2ed0bd6b75
Kaai Suzuki <suzydp@me.com>
2019-07-26 15:49:19 -0700

    Modify footer

--- footer.php
+++ footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'liber-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'liber-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'liber-theme' ), 'liber-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p class="footer__copyright">&copy; Timelag.ltd All rights reserved.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
