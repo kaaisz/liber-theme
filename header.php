3281d9cfbf7625ced5aa45f9ef2049a8ba1dee3c
Kaai Suzuki <suzydp@me.com>
2019-07-28 18:46:19 -0700

    Delete unnecessary line on header

--- header.php
+++ header.php
@@ -28,21 +28,15 @@
 	<header id="masthead" class="header site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+			?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$liber_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $liber_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $liber_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			<?php	endif; ?>
 		</div><!-- .site-branding -->
 		<nav id="site-navigation" class="nav main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'liber-theme' ); ?></button>
