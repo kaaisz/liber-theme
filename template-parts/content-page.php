a80ec27ebb48b96c98c1e31ebb342778ca3bcd18
Kaai Suzuki <suzydp@me.com>
2019-07-28 18:34:59 -0700

    Modify content page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,43 +10,41 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
 	<?php liber_theme_post_thumbnail(); ?>
 
-	<div class="entry-content">
+	<section class="entry-content">
+		<div class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div><!-- .entry-header -->
 		<?php
-		the_content();
+			the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'liber-theme' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'liber-theme' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'liber-theme' ),
+				'after'  => '</div>',
+			) );
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+			<?php if ( get_edit_post_link() ) : ?>
+			<div class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'liber-theme' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</div><!-- .entry-footer -->
+		<?php endif; ?>
+	</section><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
