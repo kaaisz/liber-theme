5ff940245d801edeced101265f486167ff6f5cce
Kaai Suzuki <suzydp@me.com>
2019-07-28 19:13:14 -0700

    Modify header styles

--- header.php
+++ header.php
@@ -31,22 +31,30 @@
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
 			?>
+				<nav id="site-navigation" class="nav main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'liber-theme' ); ?></button>
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						) );
+					?>
+				</nav><!-- #site-navigation -->
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
+			<?php else : ?>
+				<nav id="site-navigation" class="nav main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'liber-theme' ); ?></button>
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class'        => 'nav__menus',
+						) );
+					?>
+				</nav><!-- #site-navigation -->
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php	endif; ?>
 		</div><!-- .site-branding -->
-		<nav id="site-navigation" class="nav main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'liber-theme' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
