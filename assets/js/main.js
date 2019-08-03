5eefde78e33b1fda7390587271a3c69315b14b81
Kaai Suzuki <suzydp@me.com>
2019-08-02 20:57:04 -0700

    Add main.js

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+window.onload = function () {
+  let width = 0, height = 0;
+  if( typeof( window.innerWidth ) == 'number' ) {
+    // if not IE
+    width = window.innerWidth;
+    height = window.innerHeight;
+  } else if ( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+    // IE6+
+    width = document.documentElement.clientWidth;
+    height = document.documentElement.clientHeight;
+  } else if ( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+    width = document.body.clientWidth;
+    height = document.body.clientHeight;
+  }
+
+  console.log('width', width);
+  console.log('height', height);
+}
+
+
