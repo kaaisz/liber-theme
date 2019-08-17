951aa1f1653dff679c2e86df660e83f88ff59595
Kaai Suzuki <suzydp@me.com>
2019-08-17 15:49:30 -0700

    Make condition for header in content page

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,18 +10,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('article-page'); ?>>
+
+	<!-- invoke scroll indicator -->
+	<?php get_template_part( 'template-parts/intro-scroll-indicator', 'none' )?>
+
 	<header class="entry-header">
-	<?php the_post_thumbnail('large', array('class' => 'article-page__img')); ?>
-	<div class="intro__scroll">
-		<?php if (ua_smartphone() == true): ?>
-			<p class="intro__scroll-text">Swipe to left</p>
-		<?php else: ?>
-			<p class="intro__scroll-text">Scroll to left</p>
-		<?php endif;?>
-		<span class="intro__scroll-bar"></span>
-		<span class="intro__scroll-bar"></span>
-		<span class="intro__scroll-bar"></span>
-	</div>
+		<?php if (has_post_thumbnail()):?>
+			<?php the_post_thumbnail('large', array('class' => 'article-page__img'));?>
+		<?php else : ?>
+			<div class="article-page__img">
+		<?php endif; ?>
+		
 		<?php	if ( is_singular() ) :?>
 			<div class="article-page__title intro">
 				<h1 class="article-page__title_main"><?php the_title();?></h1>
@@ -32,11 +31,13 @@
 				<h2 class="entry-title article-page__title"><a href="<?php get_permalink();?>" rel="bookmark"><?php the_title();?></a></h2>
 				<div class="article-page__title_subs"><?php the_field('article_sub_title'); ?></div>
 			</div>
-		<?php 
-		endif;
+		<?php endif;?>
 
-		if ( 'post' === get_post_type() ) :
-			?>
+		<?php if ( 'post' === get_post_type() ) :?>
+		<?php endif; ?>
+
+		<?php if (!has_post_thumbnail()):?>
+			</div><!-- .article-page__img -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
