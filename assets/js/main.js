a94cdd8310b5b782c86a84dc62a130d9a8c53b01
Kaai Suzuki <suzydp@me.com>
2019-08-19 21:03:50 -0700

    Re-order js codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.onload = function () {
+
+  console.log('userAgent', navigator.userAgent);
   let width = 0, height = 0;
   if( typeof( window.innerWidth ) == 'number' ) {
     // if not IE
@@ -18,6 +20,24 @@ window.onload = function () {
   console.log('width', width);
   console.log('height', height);
 
+  // get viewport height and multiple it by 1% to get a value for vh unit
+  let vh = window.innerHeight * 0.01;
+
+  // set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // listen to the resize event
+  window.addEventListener('resize', () => {
+    // apply same action above
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  })
+
+  // control position for scroll indicator
+  const scrollIndicator = document.querySelector('.intro__scroll');
+  const trigger = document.querySelector('.indicator-trigger');
+  const edgeOfIntro = trigger.offsetWidth;
+
   function hideIndicator() {
     // debug
     // console.log(edgeOfIntro, window.scrollX);
@@ -25,7 +45,6 @@ window.onload = function () {
     // when scroll indicator has reached to section class = main
     if (-(window.scrollX) >= edgeOfIntro) {
       // hide scroll indicator
-  
       // debug
       // console.log('hide indicator');
       scrollIndicator.style.opacity = '0';
@@ -36,62 +55,43 @@ window.onload = function () {
   
   // console will change while scroll is active
   window.addEventListener('scroll', hideIndicator);
-  
-}
 
-// get viewport height and multiple it by 1% to get a value for vh unit
-let vh = window.innerHeight * 0.01;
+  // hamburger menu
+  const body = document.querySelector('body');
+  const hamburger = document.querySelector('.nav__toggle');
+  const content = document.querySelector('#content');
+  const drawer = document.querySelector('.nav__drawer');
+  const overlay = document.querySelector('.nav__overlay');
 
-// set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// listen to the resize event
-window.addEventListener('resize', () => {
-  // apply same action above
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-})
-
-// prevent enable vertical scroll
-const scrollOff = function (e) {
-  e.preventDefault();
-}
-document.body.addEventListener('touchmove', scrollOff, false);
-document.body.removeEventListener('touchmove', scrollOff, false);
-
-
-// hamburger menu
-const body = document.querySelector('body');
-const hamburger = document.querySelector('.nav__toggle');
-const drawer = document.querySelector('.nav__drawer');
-const overlay = document.querySelector('.nav__overlay');
-
-hamburger.addEventListener('click', function (e){
-  // fixed background when overlay is enable
-  if(body.style.overflowX == 'hidden') {
-    body.style.overflowX = 'scroll';
-  } else {
-    body.style.overflowX = 'hidden';
+  // prevent enable vertical scroll
+  const scrollOff = function (e) {
+    e.preventDefault();
   }
-  hamburger.classList.toggle('active');
-  // drawer open
-  drawer.classList.toggle('open');
-  // enable overlay
-  overlay.classList.toggle('active');
-});
+  document.body.addEventListener('touchmove', scrollOff, false);
+  // document.body.removeEventListener('touchmove', scrollOff, false);
 
-overlay.addEventListener('click', function(){
-  hamburger.classList.remove('active');
-  // drawer open
-  drawer.classList.remove('open');
-  // enable overlay
-  overlay.classList.remove('active');
-  body.style.overflowX = 'scroll';
-  e.preventDefault();
-});
+  hamburger.addEventListener('click', function (e){
+    // fixed background when overlay is enable
 
-// control position for scroll indicator
-const scrollIndicator = document.querySelector('.intro__scroll');
-const trigger = document.querySelector('.indicator-trigger');
-const edgeOfIntro = trigger.offsetWidth;
+    if(body.style.overflowX == 'hidden') {
+      body.style.overflowX = 'scroll';
+    } else {
+      body.style.overflowX = 'hidden';
+    }
+    hamburger.classList.toggle('active');
+    // drawer open
+    drawer.classList.toggle('open');
+    // enable overlay
+    overlay.classList.toggle('active');
+  });
 
+  overlay.addEventListener('click', function(){
+    hamburger.classList.remove('active');
+    // drawer open
+    drawer.classList.remove('open');
+    // enable overlay
+    overlay.classList.remove('active');
+    body.style.overflowX = 'scroll';
+    e.preventDefault();
+  });
+}
