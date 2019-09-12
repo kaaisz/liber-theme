2e17d42d04514905dff99a802331395527ffe45a
Kaai Suzuki <suzydp@me.com>
2019-09-12 02:15:36 -0700

    Activate SNS button

--- header.php
+++ header.php
@@ -13,6 +13,23 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php if ( is_front_page() && is_home() ) :?>
+		<meta property="og:locale" content="ja_JP" />
+		<meta property="og:site_name" content="リベル" />
+		<meta property="og:url"           content="https://liber.community/" />
+		<meta property="og:type"          content="website" />
+		<meta property="og:title"         content="リベル" />
+		<meta property="og:description"   content="広く深い知の旅へ、いざ。" />
+		<meta property="og:image"         content="https://www.your-domain.com/path/image.jpg" />
+	<?php else: ?>
+		<meta property="og:locale" content="ja_JP" />
+		<meta property="og:site_name" content="リベル" />
+		<meta property="og:url"           content="<?php the_permalink(); ?>" />
+		<meta property="og:type"          content="website" />
+		<meta property="og:title"         content="<?= the_title();?>〜<?= the_field('article_sub_title'); ?>〜 | リベル" />
+		<meta property="og:description"   content="広く深い知の旅へ、いざ。" />
+		<meta property="og:image"         content="https://www.your-domain.com/path/image.jpg" />
+	<?php endif; ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/css/main.css" />
