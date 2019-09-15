66bd9bc7ac0f74d33268206f7f94c4e44ecdaca0
Kaai Suzuki <suzydp@me.com>
2019-09-15 15:19:58 -0700

    Apply styles for 404 page

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'liber-theme' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'ページが見つかりません', 'liber-theme' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
