be3c0746990112e82e679f16b10511d4cac05a08
Kaai Suzuki <suzydp@me.com>
2019-09-15 01:20:15 -0700

    Refactor codes with optimizing smartphone view

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php liber_theme_post_thumbnail(); ?>
 
-	<section class="entry-content">
+	<section class="entry-content page">
 		<div class="entry-header">
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</div><!-- .entry-header -->
