935c7b5d7b385e09c09af1567d98b01c59705162
Kaai Suzuki <suzydp@me.com>
2020-02-28 22:53:44 -0800

    Apply new table of contents style with interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,98 @@
-'use strict';
+(function () {
+	'use strict';
 
-const userAgent = window.navigator.userAgent.toLowerCase();
+	const userAgent = window.navigator.userAgent.toLowerCase();
 
-// get viewport height and multiple it by 1% to get a value for vh unit
-let vh = window.innerHeight * 0.01;
-// set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-// listen to the resize event
-window.addEventListener('resize', () => {
-  // apply same action above
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-})
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	window.addEventListener('resize', () => {
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	})
 
-// Apply lazyload
-const lazyLoad = new LazyLoad({
-  elements_selector: ".lazy"
-});
+	const lazyLoad = new LazyLoad({
+		elements_selector: ".lazy"
+	});
 
-// hamburger menu
-const body = document.querySelector('body');
-const hamburger = document.querySelector('.nav__toggle');
-const content = document.querySelector('#content');
-const drawer = document.querySelector('.nav__drawer');
-const overlay = document.querySelector('.nav__overlay');
+	controlNav(); // hamburger menu
+	controlScroll(); // scroll action
+	
+	function controlNav() {
+		const body = document.querySelector('body');
+		const hamburger = document.querySelector('.nav__toggle');
+		const drawer = document.querySelector('.nav__drawer');
+		const overlay = document.querySelector('.nav__overlay');
 
-hamburger.addEventListener('click', function (e){
-  // fixed background when overlay is enable
-  e.preventDefault();
-  if(body.style.overflowX == 'hidden') {
-    body.style.overflowX = 'scroll';
-  } else {
-    body.style.overflowX = 'hidden';
-  }
-  hamburger.classList.toggle('active');
-  // drawer open
-  drawer.classList.toggle('open');
-  // enable overlay
-  overlay.classList.toggle('active');
-});
+		hamburger.addEventListener('click', function (e){
+			// fixed background when overlay is enable
+			e.preventDefault();
+			if(body.style.overflowX == 'hidden') {
+				body.style.overflowX = 'scroll';
+			} else {
+				body.style.overflowX = 'hidden';
+			}
+			hamburger.classList.toggle('active');
+			drawer.classList.toggle('open');
+			overlay.classList.toggle('active');
+		});
+	
+		overlay.addEventListener('click', function(e){
+			e.preventDefault();
+			hamburger.classList.remove('active');
+			drawer.classList.remove('open');
+			overlay.classList.remove('active');
+			body.style.overflowX = 'scroll';
+		});
+	}
 
-overlay.addEventListener('click', function(e){
-  e.preventDefault();
-  hamburger.classList.remove('active');
-  drawer.classList.remove('open');
-  overlay.classList.remove('active');
-  body.style.overflowX = 'scroll';
-});
+	function controlScroll() {
+		let target = document.querySelector('.site');
+		const scrollIndicator = document.querySelector('.indicator');
+		const trigger = document.querySelector('.indicator-trigger');
+		const targetWidth = target.scrollLeft;
+		const triggerFlag = target.offsetWidth;
+		const edgeOfIntro = trigger.offsetWidth;
+		const contentLength = parseInt(targetWidth + edgeOfIntro);
+		const tocTab = document.querySelector('#ez-toc-container');
+		const toggler = document.querySelector('.ez-toc-title-toggle');
 
-// control position for scroll indicator
-let target = document.querySelector('.site');
-const scrollIndicator = document.querySelector('.indicator');
-const trigger = document.querySelector('.indicator-trigger');
-const targetWidth = target.scrollLeft;
-const triggerFlag = target.offsetWidth;
-const edgeOfIntro = trigger.offsetWidth;
-const contentLength = parseInt(targetWidth + edgeOfIntro);
+		if(target) {
+			target.addEventListener('scroll', controlIndicator);
+		}
+	
+		function controlIndicator() {
+			// console.log('edgeOfIntro: ', edgeOfIntro, 'scroll: ', target.scrollLeft);
+			// console.log('rest of scroll until edge: ', (target.scrollLeft - edgeOfIntro));
+	
+			if (target.scrollLeft <= (contentLength - triggerFlag)) {
+				if(scrollIndicator) {
+					scrollIndicator.style.opacity = '0';
+				}
+				if(tocTab) {
+					toggler.classList.add('show');
+				}
+			} else {
+				if(tocTab) {
+					toggler.classList.remove('show');
+				}
+			}
+		}
 
-target.addEventListener('scroll', function() {
-  hideIndicator();
-});
+		tocTab.querySelectorAll('nav ul a').forEach(el => {
+			el.addEventListener('click', toggleContents);
+		});
 
-function hideIndicator() {
-  // console.log('edgeOfIntro: ', edgeOfIntro, 'scroll: ', target.scrollLeft);
-  // console.log('rest of scroll until edge: ', (target.scrollLeft - edgeOfIntro));
-  if (target.scrollLeft <= (contentLength - triggerFlag)) {
-    scrollIndicator.style.opacity = '0';
-  }
-}
+		toggler.innerHTML = '目次';
+		toggler.addEventListener('click', toggleContents);
+
+		function toggleContents() {
+			if(tocTab.classList.contains('open')) {
+				tocTab.classList.remove('open')
+				toggler.innerHTML = '目次';
+			} else {
+				tocTab.classList.add('open')
+				toggler.innerHTML = '閉じる';
+			}
+		}
+	}
+})();
